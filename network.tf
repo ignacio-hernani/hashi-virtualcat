ef71416ed3ad9b6ae4aae7853dbfef985a9c7cb9
ignacio-hernani <h.ignacio@pm.me>
2025-05-06 22:07:45 +0200

    new static subnet

--- network.tf
+++ network.tf
@@ -25,14 +25,14 @@ resource "aws_subnet" "hashicat" {
   }
 }
 
-#resource "aws_subnet" "static_subnet" {
-#  vpc_id     = aws_vpc.hashicat.id
-#  cidr_block = "10.100.10.0/24"
-#
-#  tags = {
-#    name = "${random_string.preffix.result}-static-subnet"
-#  }
-#}
+resource "aws_subnet" "static_subnet" {
+  vpc_id     = aws_vpc.hashicat.id
+  cidr_block = "10.100.10.0/24"
+
+  tags = {
+    name = "${random_string.preffix.result}-static-subnet"
+  }
+}
 
 resource "aws_route_table" "hashicat" {
   vpc_id = aws_vpc.hashicat.id
